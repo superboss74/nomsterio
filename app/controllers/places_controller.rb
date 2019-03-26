9d13641e080bb4a3bde4409a964d185dc925a07a
Hugo Mejia <superboss74@protonmail.com>
2019-03-26 06:16:20 +0000

    Adding Edit user Permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
 def index
   # or, use an explicit "per page" limit:
@@ -25,10 +25,18 @@ end
 
 def edit
   @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
 end
 
 def update
   @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+  
   @place.update_attributes(place_params)
   redirect_to root_path
 end
