104e0c83fb314b3e6bdd5d519194153d2806f7b7
Hugo Mejia <superboss74@protonmail.com>
2019-03-31 13:01:57 +0000

    Adding Comments to Database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address, presence: true
   after_validation :geocode, presence: true
+  
   validates :name, presence: true, length: {maximum:140, minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
