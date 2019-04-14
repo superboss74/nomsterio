8ba5f26c3242d2362625a3848fbe59ba8e291ffb
Hugo Mejia <superboss74@protonmail.com>
2019-04-14 19:34:01 +0000

    Previewing email in Dev Environment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
 
     def comment_added
-        mail(to: "superboss74@protonmail.com",
+        mail(to: "hugo.mejia@acrolinx.com",
             subject: "A comment has been added to your place")
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,22 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+  config.action_mailer.perform_deliveries = true
+   # Don't care if the mailer can't send.
+   config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -30,8 +46,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+ 
 
   config.action_mailer.perform_caching = false
 
@@ -50,7 +65,7 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  config.assets.quiet = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -59,6 +74,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+  
   
 end
